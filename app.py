6c945821ad2b43ca7eb89ef53c3ba87087872652
xckev <xckevin@outlook.com>
2024-10-13 08:59:02 -0700

    cleaning up

--- app.py
+++ app.py
@@ -8,15 +8,13 @@ import io
 import numpy as np
 import cv2
 
+# from peft import PeftModel, PeftConfig
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 
-
-from peft import PeftModel, PeftConfig
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-config = PeftConfig.from_pretrained("yuriachermann/Not-so-bright-AGI-Llama3.1-8B-UC200k-v2")
-base_model = AutoModelForCausalLM.from_pretrained("VAGOsolutions/Llama-3-SauerkrautLM-8b-Instruct")
-model = PeftModel.from_pretrained(base_model, "yuriachermann/Not-so-bright-AGI-Llama3.1-8B-UC200k-v2")
-tokenizer = AutoTokenizer.from_pretrained("VAGOsolutions/Llama-3-SauerkrautLM-8b-Instruct")
+# config = PeftConfig.from_pretrained("yuriachermann/Not-so-bright-AGI-Llama3.1-8B-UC200k-v2")
+# base_model = AutoModelForCausalLM.from_pretrained("VAGOsolutions/Llama-3-SauerkrautLM-8b-Instruct")
+# model = PeftModel.from_pretrained(base_model, "yuriachermann/Not-so-bright-AGI-Llama3.1-8B-UC200k-v2")
+# tokenizer = AutoTokenizer.from_pretrained("VAGOsolutions/Llama-3-SauerkrautLM-8b-Instruct")
 
 app = Flask(__name__)
 
@@ -28,12 +26,6 @@ def get_home():
     toReturn = jsonify({"text": "Welcome to the Triage backend server! API requests may be requested from this URL."})
     toReturn.headers.add('Access-Control-Allow-Origin', '*')
     return toReturn
-
-
-def lastweek(string=True):
-    day = datetime.date.today() - datetime.timedelta(7)
-    return day
-
 
 @app.route('/getRisk/<string:address>', methods=['GET'])
 def getRisk(address):
@@ -78,38 +70,30 @@ def getRisk(address):
     toReturn.headers.add('Access-Control-Allow-Origin', '*')
     return toReturn
 
-@app.route('/intel-llama-question/<string:disasterName>/<string:query>', methods=['GET'])
-def intel_llama_question(disasterName, query):
-    prompt = "You are Triage, a real-time disaster detection and relief AI assistant to answer questions regarding " + disasterName + ". You will be provided summaries of recent and relevant news sources that may help inform your responses. Keep your answers relevant and helpful as the user may currently be involved in a disaster. Decline answering unrelated questions.\n\nUser query: " + query + "\n\nNEWS ARTICLES:\n"
+# @app.route('/intel-llama-question/<string:disasterName>/<string:query>', methods=['GET'])
+# def intel_llama_question(disasterName, query):
+#     prompt = "You are Triage, a real-time disaster detection and relief AI assistant to answer questions regarding " + disasterName + ". You will be provided summaries of recent and relevant news sources that may help inform your responses. Keep your answers relevant and helpful as the user may currently be involved in a disaster. Decline answering unrelated questions.\n\nUser query: " + query + "\n\nNEWS ARTICLES:\n"
 
-    news = get_news(disasterName)
-    articles = news.json["articles"]
-    #print(articles)
-    for article in articles:
-        prompt += "\nArticle Title: " + article["title"] + "\nArticle Description: " + article["description"] + "\nArticle Content: " + article["content"] + "\n\n"
+#     news = get_news(disasterName)
+#     articles = news.json["articles"]
+#     #print(articles)
+#     for article in articles:
+#         prompt += "\nArticle Title: " + article["title"] + "\nArticle Description: " + article["description"] + "\nArticle Content: " + article["content"] + "\n\n"
 
-    print("prompt made!")
-    inputs = tokenizer(prompt, return_tensors="pt")
-    print("tokenized!")
-    output_tokens = model.generate(**inputs, max_length=500)
-    print("generated!")
-    response = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
-    print("decoded!")
+#     print("prompt made!")
+#     inputs = tokenizer(prompt, return_tensors="pt")
+#     print("tokenized!")
+#     output_tokens = model.generate(**inputs, max_length=500)
+#     print("generated!")
+#     response = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+#     print("decoded!")
 
-    toReturn = jsonify({"answer": response})
-    toReturn.headers.add('Access-Control-Allow-Origin', '*')
-    return toReturn
+#     toReturn = jsonify({"answer": response})
+#     toReturn.headers.add('Access-Control-Allow-Origin', '*')
+#     return toReturn
 
 @app.route('/perplexity-question/<string:disasterName>/<string:query>', methods=['GET'])
 def perplexity_question(disasterName, query):
-    '''
-    lweek = lastweek(True)
-    articles = news_api.get_everything(q=disasterName, from_param=lweek, sort_by="relevancy", page_size=5, page=1)
-
-    arr = articles["articles"]
-    print(type(arr))
-    print(arr)
-    '''
     url = "https://api.perplexity.ai/chat/completions"
     payload = {
     "model": "llama-3.1-sonar-small-128k-online",
